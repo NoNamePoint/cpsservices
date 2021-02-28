4cef9e3b9b3aa25e1f1fe1b1a151ce1c14fc3a4a
NoNamePoint <krassergey-37@mail.ru>
2021-02-28 21:05:45 +0300

    1.6 верстка

--- src/js/index.js
+++ src/js/index.js
@@ -16,12 +16,17 @@ if (window.innerWidth <= 767) {
 
 let contentBrands = document.querySelector('.brands');
 let buttonBrands = document.querySelector('.brands__button');
-let showMoreButtonLink = document.querySelector('.showmore-button__link');
+let showMoreButtonLink = document.querySelector('.brands__button-link');
+let headerButton = document.querySelector('.functional-btns__button');
+let menuClose = document.querySelector('.functional-btns__menu');
+let blur = document.querySelector('.blur');
+let menu = document.querySelector('.menu');
 
 
 
 function showMoreLess() { 
     let contentBrandsHeight = getElementStyle("max-height", contentBrands)
+    console.log(contentBrandsHeight)
 
     if (contentBrandsHeight != "none") {
         contentBrands.style.maxHeight = "none";
@@ -42,12 +47,37 @@ function getElementStyle(style, element){
 };
 
 function brandsButtonToggleClass(){
-    showMoreButtonLink.classList.toggle("showmore-button__link--showless");
+    showMoreButtonLink.classList.toggle("brands__button-link--showless");
     buttonBrands.classList.toggle("brands__button--less");
 };
 
+function headerToggleClass() {
+    menu.classList.toggle("menu--active");
+    blur.classList.toggle("blur--active");
+};
 
-buttonBrands.addEventListener('click', showMoreLess);
+
+
+buttonBrands.addEventListener('click', (e)=>{
+    e.preventDefault();
+    showMoreLess();
+});
+
+
+
+headerButton.addEventListener('click', function(){
+    headerToggleClass(); 
+});
+
+
+menuClose.addEventListener('click', function () {  
+
+    
+    menu.classList.remove("menu--active");
+    blur.classList.remove("blur--active");
+     
+});
+
 
 
 
